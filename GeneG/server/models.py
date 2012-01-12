45c435b6be93acd1492368639ed62541c61f60aa
ishai <ishai@empeeric.com>
2012-01-12 15:05:49 +0200

    p_value added

--- GeneG/server/models.py
+++ GeneG/server/models.py
@@ -14,6 +14,8 @@ TEST_SOURCES = (
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     genome = models.FileField(upload_to='genomes',null=True,blank=True)
+    def __unicode__(self):
+        return unicode(self.user)
 
 
 def create_user_profile(sender, instance, created, **kwargs):
@@ -24,29 +26,36 @@ post_save.connect(create_user_profile, sender=User)
 class Phenotype(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField(max_length=755,default='')
+    def __unicode__(self):
+        return self.name
 
 class TestVariant(models.Model):
 
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30,unique=True)
     description = models.CharField(max_length=700,default='',blank=True)
     source = models.CharField(max_length=30,null=True,blank=True)
     #target_script = models.CharField(max_length=50,null=True, blank=True)
     phenotype = models.ForeignKey(Phenotype)
-    pubmed_id = models.CharField(max_length=30)
+    pubmed_id = models.CharField(max_length=30,null=True,blank=True)
+    p_value = models.FloatField(null=True,blank=True)
     meta = models.TextField(max_length=755,null=True, blank=True)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(default=datetime.now, editable=False)
 
     def __unicode__(self):
-        return self.name
+        return unicode(self.name)
 
 class PhenotypeFamily(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField(max_length=755,default='')
+    def __unicode__(self):
+        return unicode(self.name)
 
 class PhenotypeFamilyRelation(models.Model):
     family = models.ForeignKey(PhenotypeFamily)
     phenotype = models.ForeignKey(Phenotype)
+    def __unicode__(self):
+        return u'%u | %u' % (unicode(self.family),unicode(self.phenotype))
 
 
 class UserTestResult(models.Model):
